c45d50f1dda58476d76d15e058a7874362037a6e
GabrielDM <gabydemaussion@hotmail.com>
2024-08-08 18:52:07 -0300

    create proyect
    Inicio del proyecto
    
    Co-Authored-By: camilpz <111206640+camilpz@users.noreply.github.com>

---
+++ carteleraCine/src/main/java/com/personal/carteleraCine/CarteleraCineApplication.java
@@ -0,0 +1,13 @@
+package com.personal.carteleraCine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteleraCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteleraCineApplication.class, args);
+	}
+
+}
